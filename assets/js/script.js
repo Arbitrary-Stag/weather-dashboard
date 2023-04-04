bfd5e2647f6e358ddb7c18f69ae7475c6eb32c34
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-04-03 17:56:46 -0700

    added code for current weather conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,20 @@
+// Declared variables
 const API_KEY = '9d51f243a672598006179ae1583751b8';
 const fetchButton = document.getElementById('submit');
 const url = 'https://api.openweathermap.org/data/2.5/';
-const url2 ='http://api.openweathermap.org/geo/1.0/';
 const userSearch = document.getElementById("userSearch");
+const cityName = document.getElementById("city-date");
+const icon = document.getElementById("icon");
+const temp = document.getElementById("temp");
+const wind = document.getElementById("wind");
+const humidity = document.getElementById("humidity");
 
 function getApi() {
   
   let search = userSearch.value; 
-
-  fetch(`${url2}direct?q=${search}&appid=${API_KEY}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data[0].lat)
-      console.log(data[0].lon)
-    })
-
-  console.log(search);
-
-  fetch(`${url}weather?q=${search}&appid=${API_KEY}`)
+ 
+// This api request is for getting the current weather conditions of a specific city 
+  fetch(`${url}weather?q=${search}&units=imperial&appid=${API_KEY}`)
     .then(function (response) {
       return response.json();
     })
@@ -30,25 +25,28 @@ function getApi() {
       console.log(data.wind.speed)
       console.log(data.weather[0].icon)
 
+      const lat = data.coord.lat;
+      const lon = data.coord.lon;
+      const weatherimg = data.weather[0].icon;
+      const date = dayjs(data.dt * 1000).format("M/DD/YYYY");
 
-    //   //Loop over the data to generate a table, each table row will have a link to the repo url
-    //   for (var i = 0; i < data.length; i++) {
-    //     // Creating elements, tablerow, tabledata, and anchor
-    //     var createTableRow = document.createElement('tr');
-    //     var tableData = document.createElement('td');
-    //     var link = document.createElement('a');
+      cityName.textContent = data.name + " " + "(" + date + ")"; 
+      temp.textContent = "Temp: " + data.main.temp + "° F";
+      wind.textContent = "Wind Speed: " + data.wind.speed + " MPH"
+      humidity.textContent = "Humidity: " + data.main.humidity + "%";
+      icon.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherimg + ".png");
+      icon.setAttribute("style", "width: 6rem", "height: 6rem");
 
-    //     // Setting the text of link and the href of the link
-    //     link.textContent = data[i].html_url;
-    //     link.href = data[i].html_url;
-
-    //     // Appending the link to the tabledata and then appending the tabledata to the tablerow
-    //     // The tablerow then gets appended to the tablebody
-    //     tableData.appendChild(link);
-    //     createTableRow.appendChild(tableData);
-    //     tableBody.appendChild(createTableRow);
-    
-    });
+// This api request fetches the data for the 5 day forecast  
+  fetch(`${url}forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+  
+  }); 
 }
 
 fetchButton.addEventListener('click', getApi);
